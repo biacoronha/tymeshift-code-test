bc1b03d818d13ae3d0e2c00aa8e1e85e490780e6
Beatriz Coronha <biacoronha@gmail.com>
2021-08-23 18:06:21 -0300

    project structure

---
+++ src/pages/HomePage.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+
+class HomePage extends Component {
+    constructor(props) {
+        super(props);
+        const data;
+    }
+    render() {
+    }
+
+    componentWillMount() {
+    }
+}
+
+export default HomePage
