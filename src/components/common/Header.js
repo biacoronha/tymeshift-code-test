3d0dcc558abdf3da3f44b10fc431653d0bc6af8f
Beatriz Coronha <biacoronha@gmail.com>
2021-08-25 12:34:30 -0300

    css ajustments in home page

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -2,10 +2,10 @@ import React from 'react'
 import './Header.css'
 
 const Header = (props) =>(
-    <p className="header">
+    <div className="header">
        <h5 className="section-title">All Locations</h5>
        <h6 className="header-name">Company locations</h6>
-    </p>
+    </div>
 
 )
 
